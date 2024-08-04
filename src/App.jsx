9c818d2bc40e29d2e83aa769c9a47a7a0f9d0885
HaAlpaca <dangminhha5@gmai.com>
2024-08-04 17:24:50 +0700

    test theming"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Button from '@mui/material/Button'
 import { AccessAlarm } from '@mui/icons-material'
+import Typography from '@mui/material/Typography'
 function App() {
   return (
     <>
       <Button variant="contained">Contained</Button>
       <div>HaAlpaca</div>
       <AccessAlarm />
+      <Typography variant="body2" color={'text.secondary'} >
+        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+        quasi quidem quibusdam.
+      </Typography>
     </>
   )
 
