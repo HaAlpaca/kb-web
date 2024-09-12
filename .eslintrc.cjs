9b4652e6d3b106b2b87ea687161aad8f4d50d1b4
HaAlpaca <dangminhhaworkspace@gmail.com>
2024-09-13 03:38:32 +0700

    config absolute import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,13 +12,22 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: [
-    // 'prettier',
+    'prettier',
     'react',
     'react-hooks',
     'react-refresh'
   ],
   rules: {
-    // 'prettier/prettier': ['error'],
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        bracketSpacing: true,
+        trailingComma: 'none',
+        arrowParens: 'avoid',
+      }
+    ],
     // MUI
     'no-restricted-imports': [
       'error',
@@ -33,23 +42,23 @@ module.exports = {
     'react/prop-types': 0,
     'react/display-name': 0,
     // Common
-    // 'no-console': 1,
-    // 'no-lonely-if': 1,
-    // 'no-unused-vars': 1,
-    // 'no-trailing-spaces': 1,
-    // 'no-multi-spaces': 1,
-    // 'no-multiple-empty-lines': 1,
-    // 'space-before-blocks': ['error', 'always'],
-    // 'object-curly-spacing': [1, 'always'],
-    // 'indent': ['warn', 2],
-    // 'semi': [1, 'never'],
-    // 'quotes': ['error', 'single'],
-    // 'array-bracket-spacing': 1,
-    // 'linebreak-style': 0,
-    // 'no-unexpected-multiline': 'warn',
-    // 'keyword-spacing': 1,
-    // 'comma-dangle': 1,
-    // 'comma-spacing': 1,
-    // 'arrow-spacing': 1
+    'no-console': 1,
+    'no-lonely-if': 1,
+    'no-unused-vars': 1,
+    'no-trailing-spaces': 1,
+    'no-multi-spaces': 1,
+    'no-multiple-empty-lines': 1,
+    'space-before-blocks': ['error', 'always'],
+    'object-curly-spacing': [1, 'always'],
+    'indent': ['warn', 2],
+    'semi': [1, 'never'],
+    'quotes': ['error', 'single'],
+    'array-bracket-spacing': 1,
+    'linebreak-style': 0,
+    'no-unexpected-multiline': 'warn',
+    'keyword-spacing': 1,
+    'comma-dangle': 1,
+    'comma-spacing': 1,
+    'arrow-spacing': 1
   }
 }
