93279adfb228be254694232d0c06bf3f74b9d875
HaAlpaca <dangminhhaworkspace@gmail.com>
2024-10-19 00:59:34 +0700

    call api

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -5,7 +5,7 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 
-import { mockData } from '~/apis/mock-data'
+// import { mockData } from '~/apis/mock-data'
 import { useEffect, useState } from 'react'
 
 import { fetchBoardDetailsAPI } from '~/apis'
@@ -22,8 +22,8 @@ function Board() {
     <>
       <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
         <AppBar />
-        <BoardBar board={mockData.board} />
-        <BoardContent board={mockData.board} />
+        <BoardBar board={board} />
+        <BoardContent board={board} />
       </Container>
     </>
   )
