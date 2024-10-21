e16ac54d1b214af9c5a46675138b1e8d67bb0518
HaAlpaca <dangminhhaworkspace@gmail.com>
2024-10-22 01:27:18 +0700

    fix process env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,11 @@
-export const API_ROOT = 'https://trello-api-td8m.onrender.com'
+console.log(process.env)
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-td8m.onrender.com'
+}
+export const API_ROOT = apiRoot
 // export const API_ROOT = 'https://trello-api-td8m.onrender.com'
+//http://localhost:8017/
