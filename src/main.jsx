c57435dcd40a7b506317e278ca7133b95ac44579
HaAlpaca <dangminhhaworkspace@gmail.com>
2024-11-17 20:30:34 +0700

    config redux

--- src/main.jsx
+++ src/main.jsx
@@ -7,26 +7,31 @@ import theme from './theme'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { ConfirmProvider } from 'material-ui-confirm'
+import { Provider } from 'react-redux'
+import { store } from '~/redux/store'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <CssVarsProvider theme={theme}>
-    <ConfirmProvider
-      defaultOptions={{
-        allowClose: false,
-        dialogProps: { maxWidth: 'xs' },
-        confirmationButtonProps: { color: 'secondary', variant: 'outlined' },
-        cancellationButtonProps: { color: 'inherit' }
-      }}
-    >
-      <CssBaseline />
-      <App />
-      <ToastContainer
-        theme="colored"
-        position="bottom-left"
-        // hideProgressBar="true"
-        autoClose="3000"
-      />
-    </ConfirmProvider>
-  </CssVarsProvider>
+  <Provider store={store}>
+    <CssVarsProvider theme={theme}>
+      <ConfirmProvider
+        defaultOptions={{
+          allowClose: false,
+          dialogProps: { maxWidth: 'xs' },
+          confirmationButtonProps: { color: 'secondary', variant: 'outlined' },
+          cancellationButtonProps: { color: 'inherit' }
+        }}
+      >
+        <CssBaseline />
+        <App />
+        <ToastContainer
+          theme="colored"
+          position="bottom-left"
+          // hideProgressBar="true"
+          autoClose="3000"
+        />
+      </ConfirmProvider>
+    </CssVarsProvider>
+  </Provider>
   // </React.StrictMode>
 )
---
+++ src/redux/activeBoard/activeBoardSlice.js
@@ -0,0 +1,76 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { isEmpty } from 'lodash'
+import { API_ROOT } from '~/utils/constants'
+import { generatePlaceholderCard } from '~/utils/formatters'
+// https://redux-toolkit.js.org/tutorials/quick-start
+// khoi tao gia tri 1 slice trong redux
+import { mapOrder } from '~/utils/sorts'
+const initialState = {
+  currentActiveBoard: null
+}
+// goi hanh dong bat dong bo =>
+// middleware createAsyncThunk di kem voi extraReducer
+const fetchBoardDetailsAPI = createAsyncThunk(
+  'activeBoard/fetchBoardDetailsAPI',
+  async boardId => {
+    const respond = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
+    return respond.data
+  }
+)
+
+// khoi tao Slice trong store Redux
+export const activeBoardSlice = createSlice({
+  name: 'activeBoard',
+  initialState,
+  // xu li du lieu dong bo
+  reducers: {
+    // cap nhat activeBoard
+    // https://redux-toolkit.js.org/usage/immer-reducers
+    updateCurrentActiveBoard: (state, action) => {
+      // action.payload la chuan nhan du lieu dau vao cua reducer, o day chung ta gan no ra 1 bien co nghia hon
+      const fullBoard = action.payload
+
+      // xu li du lieu....
+      // update du lieu cho current active board
+      state.currentActiveBoard = fullBoard
+    }
+  },
+  // extraReducers: noi xu li du lieu bat dong bo
+  extraReducers: builder => {
+    builder.addCase(fetchBoardDetailsAPI.fulfilled, (state, action) => {
+      // action.payload chinh la cai response.data tra ve o tren
+      let board = action.payload
+
+      // xu li du lieu truoc khi luu vao redux -> giong xu li truoc ki setState
+      // sap xep luon column khong phai doi component con map lai
+      board.columns = mapOrder(board.columns, board.columnOrderIds, '_id')
+      // xu li keo tha khi vao column rong generatePlaceholderCard
+      board.columns.forEach(column => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)]
+          column.cardOrderIds = [generatePlaceholderCard(column)._id]
+        } else {
+          // sap xep luon card khong phai doi component con map lai
+          column.cards = mapOrder(column.cards, column.cardOrderIds, '_id')
+        }
+      })
+      // .......
+      // update du lieu currentActiveBoard
+      state.currentActiveBoard = board
+    })
+  }
+})
+
+// Action creators are generated for each case reducer function
+
+// Actions: la noi dung cho components ben duoi goi bang dispatch() toi de cap nhat lai du lieu thong qua reducer (chay dong bo)
+// de y o tren thi khong co props actions dau ca, actions duoc redux tao tu dong
+export const { updateCurrentActiveBoard } = activeBoardSlice.actions
+// selectors: la noi cho cac component goi bang hook useSelector de lay du lieu ra ngoai tu trong kho redux store ra su dung
+export const selectCurrentActiveBoard = state => {
+  return state.activeBoard.currentActiveBoard
+}
+// file nay ten la activeBoardSlice nhung chung ta se export ra 1 reducer
+// export default activeBoardSlice.reducer
+export const activeBoardReducer = activeBoardSlice.reducer
