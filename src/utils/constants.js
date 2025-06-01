415cbab86320f79b820d1ba90be658eedc9552c2
HaAlpaca <dangminhhaworkspace@gmail.com>
2025-06-01 15:57:35 +0700

    change api root

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api-td8m.onrender.com'
+  apiRoot = 'https://kb-api-kfl3.onrender.com'
 }
 export const API_ROOT = apiRoot
 // export const API_ROOT = 'https://trello-api-td8m.onrender.com'
